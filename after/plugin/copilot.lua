fa7f2d63b89dfbaad54d48308cc1241f407d4fd5
José Morano <j.morano@udc.es>
2023-02-26 14:53:27 +0100

    Just updated copilot config syntax.

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,12 +1,17 @@
 local map = vim.keymap.set
 
-local function SuggestOneWord()
+local function accept_word()
   vim.fn['copilot#Accept']("")
   local bar = vim.fn['copilot#TextQueuedForInsertion']()
   return vim.fn.split(bar,  [[[ .]\zs]])[1]
 end
-map('i', '<M-j>', '<Plug>(copilot-next)')
-map('i', '<M-k>', '<Plug>(copilot-previous)')
+
 vim.g.copilot_no_tab_map = true
-vim.api.nvim_set_keymap("i", "<M-i>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-map('i', '<M-o>', SuggestOneWord, {expr = true, remap = false})
+
+map('i', '<M-j>', '<Plug>(copilot-next)', {})
+map('i', '<M-k>', '<Plug>(copilot-previous)', {})
+map(
+  "i", "<M-i>", 'copilot#Accept("<CR>")',
+  { silent = true, expr = true }
+)
+map('i', '<M-o>', accept_word, {expr = true, remap = false})
