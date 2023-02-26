661dc471b734639deacd656da2824f673874cb16
José Morano <j.morano@udc.es>
2023-02-26 15:21:47 +0100

    Copilot: added func. to accept line.

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -6,12 +6,19 @@ local function accept_word()
   return vim.fn.split(bar,  [[[ .]\zs]])[1]
 end
 
+local function accept_line()
+  vim.fn['copilot#Accept']("")
+  local bar = vim.fn['copilot#TextQueuedForInsertion']()
+  return vim.fn.split(bar,  [[[\n]\zs]])[1]
+end
+
 vim.g.copilot_no_tab_map = true
 
 map('i', '<M-j>', '<Plug>(copilot-next)', {})
 map('i', '<M-k>', '<Plug>(copilot-previous)', {})
 map(
-  "i", "<M-i>", 'copilot#Accept("<CR>")',
+  "i", "<M-p>", 'copilot#Accept("<CR>")',
   { silent = true, expr = true }
 )
 map('i', '<M-o>', accept_word, {expr = true, remap = false})
+map('i', '<M-i>', accept_line, {expr = true, remap = false})
