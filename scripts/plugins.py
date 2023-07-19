ab548417288002ba4355c283f7b008d5f310398a
José Morano <research.msj@gmail.com>
2023-07-19 17:06:50 +0200

    Do not support branches for pluging manager. Too complicated and never used.

--- scripts/plugins.py
+++ scripts/plugins.py
@@ -51,6 +51,10 @@ if sys.argv[1] == 'list':
         plugin_url = plugin_url_split[-2] + '/' + plugin_url_split[-1]
         print('  -', plugin_url, flush=True)
 
+elif sys.argv[1] == 'list-paths':
+    for plugin_path in get_plugin_paths(True):
+        print('  -', plugin_path, flush=True)
+
 elif sys.argv[1] == 'sync':
     operations = 0
     # Read plugins from file and install them if they are not installed
@@ -61,46 +65,25 @@ elif sys.argv[1] == 'sync':
         plugin = plugin.strip()
         if plugin.startswith('#'):
             continue
-        if '@' in plugin:
-            parts = plugin.split('@')
-            plugin = parts[0]
-            branch = parts[1]
-        else:
-            branch = None
         plugin_url = 'https://github.com/' + plugin
         plugin_name = plugin.split('/')[-1]
         plugin_names.append(plugin_name)
         if not os.path.exists(os.path.join(base_plugin_path, 'start', plugin_name)):
+            # Install plugin
             print(f'Installing {plugin}', flush=True)
-            if branch is not None:
-                command = ['git', 'clone', '-b', branch, plugin_url]
-            else:
-                command = ['git', 'clone', plugin_url]
             result = run(
-                command=command,
+                command=['git', 'clone', plugin_url],
                 cwd=os.path.join(base_plugin_path, 'start'),
             )
             operations += 1
         else:
             # Update plugin
             print(f'Updating {plugin}', flush=True)
-            if branch is not None:
-                try:
-                    run(
-                        ['git', 'checkout', branch],
-                        cwd=os.path.join(base_plugin_path, 'start', plugin_name),
-                    )
-                except subprocess.CalledProcessError:
-                    run(
-                        ['git', 'pull', 'origin', branch],
-                        cwd=os.path.join(base_plugin_path, 'start', plugin_name),
-                    )
-            else:
-                run(
-                    ['git', 'pull'],
-                    cwd=os.path.join(base_plugin_path, 'start', plugin_name),
-                )
-                operations += 1
+            run(
+                ['git', 'pull'],
+                cwd=os.path.join(base_plugin_path, 'start', plugin_name),
+            )
+            operations += 1
     plugin_paths = get_plugin_paths()
     # Remove plugins that are not in the plugins file
     for plugin_path in plugin_paths:
