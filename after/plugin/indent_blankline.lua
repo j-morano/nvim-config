e020f8620bc37f3febdb3632222334a604458b3e
José Morano <research.msj@gmail.com>
2023-09-28 16:04:36 +0200

    Updated indent_blank_line.

--- after/plugin/indent_blankline.lua
+++ after/plugin/indent_blankline.lua
@@ -1,12 +1,16 @@
-
-require("indent_blankline").setup {
-  char_highlight_list = {
+local highlight = {
     "IndentBlanklineIndent1",
     "IndentBlanklineIndent2",
     "IndentBlanklineIndent3",
     "IndentBlanklineIndent4",
     "IndentBlanklineIndent5",
     "IndentBlanklineIndent6",
-  },
 }
 
+require("ibl").setup {
+  indent = { highlight = highlight, char = "│" },
+  whitespace = {
+    highlight = highlight,
+    remove_blankline_trail = false,
+  },
+}
