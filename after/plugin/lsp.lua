0e1a562cc6ecebf3e20b34e34cbaa8c59b8da3ac
José Morano <research.msj@gmail.com>
2023-11-09 12:02:11 +0100

    Nvim. Changed hint_prefix for LSP signature.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -167,6 +167,7 @@ require'lspconfig'.lua_ls.setup {
 require "lsp_signature".setup({
   floating_window = false,
   doc_lines = 1,
+  hint_prefix = "📃 ",
 })
 vim.keymap.set({ 'i' }, '<M-k>', function()
   vim.lsp.buf.signature_help()
