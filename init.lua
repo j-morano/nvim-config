f52d4cec84f1b975d6d5af79522f538c6719f3db
José Morano <research.msj@gmail.com>
2023-11-15 14:43:04 +0100

    More visible comments in lecture mode.

--- init.lua
+++ init.lua
@@ -10,4 +10,5 @@ function Lec()
   vim.diagnostic.config({ virtual_text = false })
   vim.cmd('Copilot disable')
   vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { fg = "#ff0000", underline = true })
+  vim.api.nvim_set_hl(0, "Comment", { fg = "#797fa3" })
 end
