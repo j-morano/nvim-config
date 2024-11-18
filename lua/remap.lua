610ff15e74c39a96c18322decbcfd1703b4f8b76
José Morano <research.msj@gmail.com>
2024-11-18 13:48:55 +0100

    Very simple function to add fake padding

--- lua/remap.lua
+++ lua/remap.lua
@@ -71,6 +71,23 @@ map('n', 'vv', '<C-v>', opts)
 -- Alternative escape
 map('i', 'jj', '<ESC>', opts)
 
+
+local function toggle_padding()
+  -- print(vim.opt.foldcolumn)
+  local foldcolumn = vim.o.foldcolumn
+  if foldcolumn == '1' then
+    vim.opt.foldcolumn = '9'
+    vim.opt.signcolumn = 'yes:9'
+  else
+    vim.opt.foldcolumn = '1'
+    vim.opt.signcolumn = 'yes'
+  end
+end
+
+map('n', '<M-+>', toggle_padding, opts)
+
+
+
 -- Search
 local function do_hlsearch()
   vim.opt.hlsearch = true
