e6796c20f582dd5bf6d5836be2e49e3641105192
José Morano <j.morano@tuta.io>
2025-08-28 10:24:24 +0200

    copilot: accept 4 words at a time

--- after/plugin/copilot.lua
+++ after/plugin/copilot.lua
@@ -1,10 +1,14 @@
 local map = vim.keymap.set
 
-local function accept_word()
-  vim.fn['copilot#Accept']("")
+
+local function accept_words(n)
+  vim.fn['copilot#Accept']("")  -- Accept the suggestion
   local bar = vim.fn['copilot#TextQueuedForInsertion']()
-  return vim.fn.split(bar, [[[ .]\zs]])[1]
+  local words = vim.fn.split(bar, [[[ .]\zs]])  -- Split by space or dot
+  local result = table.concat(vim.list_slice(words, 1, n), "")
+  return result
 end
+
 
 local function accept_line()
   vim.fn['copilot#Accept']("")
@@ -16,8 +20,9 @@ local opts = { remap = false, silent = true, expr = true, replace_keycodes = fal
 
 map('i', '<M-m>', '<Plug>(copilot-next)', {})
 map("i", "<M-p>", 'copilot#Accept("<CR>")', opts)
-map('i', '<M-o>', accept_word, opts)
+map('i', '<M-o>', function() return accept_words(1) end, opts)
 map('i', '<M-i>', accept_line, opts)
+map('i', '<M-u>', function() return accept_words(4) end, opts)
 
 -- Activate copilot all filetypes
 vim.g.copilot_filetypes = {
