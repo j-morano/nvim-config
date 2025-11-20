ac73a92f2e24260cbedc5b16508030b2f1d81cf3
José Morano <j.morano@tuta.io>
2025-11-20 17:05:28 +0100

    Removed unnecessary 'Disable lspconfig warning for now' code

--- init.lua
+++ init.lua
@@ -12,13 +12,3 @@ function Lec()
   vim.api.nvim_set_hl(0, "DiagnosticUnderlineError", { fg = "#ff0000", underline = true })
   vim.api.nvim_set_hl(0, "Comment", { fg = "#797fa3" })
 end
-
-
--- Disable lspconfig warning for now
--- local notify = vim.notify
--- vim.notify = function(msg, level, opts)
---   if type(msg) == "string" and msg:match("The `require%('lspconfig'%)` \"framework\" is deprecated") then
---     return
---   end
---   notify(msg, level, opts)
--- end
